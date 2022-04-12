641913ec9eefde00b78e07c7521ded9f94bd1bce
luisestebanlopez <103506109+luisestebanlopez@users.noreply.github.com>
2022-04-12 13:56:55 +0100

    Update echart_main.js

--- echart_main.js
+++ echart_main.js
@@ -97,5 +97,5 @@ var getScriptPromisify = (src) => {
   }
 }
 
-  customElements.define('com-sap-sample-echarts-pie_chart_demo_xx', SamplePieChart)
+  customElements.define('com-sap-sample-echarts-pie_chart_demo_ll', SamplePieChart)
 })()
